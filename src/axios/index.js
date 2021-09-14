c420e89940ac1cc8b57b9d7fb3c50aa21b9673ae
erfanmotnt <moeini.erfan@gmail.com>
2021-09-15 00:40:02 +0430

    change repo

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 export const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'https://bankbackend.rastaiha.ir'
+    ? 'http://171.22.24.244'
     : 'http://localhost:8000'
 
 const baseAxios = Axios.create({
