3e8e349536bf6956263f61af5132d21cbcfd765a
erfanmotnt <moeini.erfan@gmail.com>
2021-09-16 22:05:48 +0430

    https

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 export const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'http://bankbackend.esfoly.ir'
+    ? 'https://bankbackend.esfoly.ir'
     : 'http://localhost:8000'
 
 const baseAxios = Axios.create({
